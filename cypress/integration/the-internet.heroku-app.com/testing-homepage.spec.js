6a25060d4f012eb24ad5ccb6e16f4d816726d59f
cheuni02 <inhcheung@gmail.com>
2021-11-23 18:18:10 -0500

    first commit

---
+++ cypress/integration/the-internet.heroku-app.com/testing-homepage.spec.js
@@ -0,0 +1,15 @@
+const listOfExercises = require('../../fixtures/listOfExercises.json')
+describe('check homepage loads with the following titles and links', () => {
+    it(`should be able to navigate to homepage with title 'Welcome to the-internet'`, () => {
+        cy.visit('https://the-internet.herokuapp.com/')
+         .get('#content h1').contains('Welcome to the-internet')
+    })
+    it(`should have all these links and their corresponding paths according to cypress/fixtures/listOfExercises.json`,() =>{
+        cy.fixture('listOfExercises').then((loeFixtures) => {
+            loeFixtures.forEach(fixture => {
+                cy.log(fixture)
+                cy.get(`#content ul li a[href="${fixture.link}"]`).should(`has.text`,fixture.label)
+            })
+        })
+    })
+})
