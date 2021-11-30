38cda82662e25d99f0173fd348a727fec15e1e67
cheuni02 <inhcheung@gmail.com>
2021-11-30 23:22:37 +0000

    added a few more GET tests to todos.spec.js

--- cypress/integration/jsonplaceholder.cypress.io/request-example/todos.spec.js
+++ cypress/integration/jsonplaceholder.cypress.io/request-example/todos.spec.js
@@ -13,4 +13,29 @@ describe('testing http requests', () => {
           })
     })
 
+    it(`Test GET request for todos #2 using 'its'`, () => {
+        cy.request(`https://jsonplaceholder.cypress.io/todos/2`)
+          .its(`body`)  
+          .should('have.property','title')
+    })
+
+    it(`Test GET request for todos #2, using 'its', and validating the whole response json`, () => {
+        cy.request(`https://jsonplaceholder.cypress.io/todos/2`)
+          .its(`body`)  
+          .should('include', {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          })
+    })
+
+    it(`Test GET request for todos #2, using 'its', and validating the one part of the response json is included/ not included`, () => {
+        cy.request(`https://jsonplaceholder.cypress.io/todos/2`)
+          .its(`body`)  
+          .should('include', {
+            "title": "quis ut nam facilis et officia qui"
+          })
+          .and('not.include', { "userId": 2 })
+    })
 })
