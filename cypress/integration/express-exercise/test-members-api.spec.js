8bcdd54d1ced1517fac475b432ef7a59facd15bd
cheuni02 <inhcheung@gmail.com>
2021-12-08 20:07:58 +0000

    added rudementary POST to members endpoint (needs refactoring)

--- cypress/integration/express-exercise/test-members-api.spec.js
+++ cypress/integration/express-exercise/test-members-api.spec.js
@@ -10,4 +10,16 @@ describe(`check HTTP requests to /api/members`, () => {
             }
         )
     })
+
+    it(`can POST a new member and verify`, () => {
+        cy.request({
+            method: 'POST',
+            url: `http://localhost:5000/api/members`,
+            body: {"name":"Alice Tang","email":"alice@gmail.com","status":"active"}
+        })
+        .then((res) => {
+            expect(res.status).to.eq(200)
+            expect(res.body).to.have.property("name","Alice Tang")
+        }) 
+    })
 })
