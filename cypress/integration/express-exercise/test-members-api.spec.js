d5eef8a5d343327eceb4e96e5c03d0c5fbdfe882
cheuni02 <inhcheung@gmail.com>
2021-12-08 19:48:57 +0000

    added test for GET all members

---
+++ cypress/integration/express-exercise/test-members-api.spec.js
@@ -0,0 +1,13 @@
+describe(`check HTP requests to /api/members`, () => {
+    it(`can GET all members`, () => {
+        cy.request({
+            method: `GET`,
+            url: `http://localhost:5000/api/members`,
+            form: true
+        }).then(
+            (res) => {
+                expect(res.body).to.have.length(8)    
+            }
+        )
+    })
+})
