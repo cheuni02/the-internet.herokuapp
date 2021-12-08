b94597b04558f9bd83516c849481a2b886a4d991
cheuni02 <inhcheung@gmail.com>
2021-12-08 00:00:02 +0000

    added baseUrl for the-innternet.herokuapp site only

--- cypress/integration/the-internet.heroku-app.com/add-remove-elements.spec.js
+++ cypress/integration/the-internet.heroku-app.com/add-remove-elements.spec.js
@@ -1,6 +1,6 @@
 describe('demonstrate that elements can be added and removed', () => {
     it(`should contain button 'Add Element'`, () => {
-        cy.visit(`https://the-internet.herokuapp.com/add_remove_elements/`)
+        cy.visit(`/add_remove_elements`)
           .get(`#content`).find(`h3`).contains(`Add/Remove Elements`)
           .get(`.example button[onclick='addElement()']`).should('have.text','Add Element')  
     })
--- cypress/integration/the-internet.heroku-app.com/testing-homepage.spec.js
+++ cypress/integration/the-internet.heroku-app.com/testing-homepage.spec.js
@@ -1,7 +1,7 @@
 const listOfExercises = require('../../fixtures/listOfExercises.json')
 describe('check homepage loads with the following titles and links', () => {
     it(`should be able to navigate to homepage with title 'Welcome to the-internet'`, () => {
-        cy.visit('https://the-internet.herokuapp.com/')
+        cy.visit('/')
          .get('#content h1').contains('Welcome to the-internet')
     })
     it(`should have all these links and their corresponding paths according to cypress/fixtures/listOfExercises.json`,() =>{
