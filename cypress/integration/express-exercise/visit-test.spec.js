d500cf604d9b73f810f4ca30dc7820edf65094e8
cheuni02 <inhcheung@gmail.com>
2021-12-28 00:28:37 +0000

    did same thing to the GUI test added script to remind user how to config url externally when doing cypress open

--- cypress/integration/express-exercise/visit-test.spec.js
+++ cypress/integration/express-exercise/visit-test.spec.js
@@ -1,6 +1,6 @@
 describe(`basic page checks`, () => {
     it(`should have these texts when visiting homepage`, () => {
-        cy.visit(`http://localhost:5000/`)
+        cy.visit(`/`)
         cy.get(`h1`).should('have.text','Test Results')
     })
 })
